c614376d6a6d336672a93f0ef943a41a4f1b0815
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-07-18 13:33:55 +0200

    prepare 1.21.8 port

--- 1.21.7/NeoForge/src/main/java/fuzs/stylisheffects/neoforge/integration/rei/NeoForgeStylishEffectsReiPlugin.java
+++
@@ -1,9 +0,0 @@
-//package fuzs.stylisheffects.neoforge.integration.rei;
-//
-//import fuzs.stylisheffects.integration.rei.StylishEffectsReiPlugin;
-//import me.shedaniel.rei.forge.REIPluginClient;
-//
-//@REIPluginClient
-//public class NeoForgeStylishEffectsReiPlugin extends StylishEffectsReiPlugin {
-//
-//}
